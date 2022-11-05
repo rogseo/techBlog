6d7c637b291fcf862dcf1ce1214c0b621fd6d1ac
rogseo <rogseo@gmail.com>
2022-11-04 21:28:29 -0500

    router.get('/post/:id') render problem solved

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,53 +1,48 @@
-const { Post,User } = require('../models');
+const { Post, User } = require('../models');
+const router = require('express').Router();
 
-const router=require('express').Router();
-
-router.get('/', async(req, res)=>{
-    try{
-        const postData=await Post.findAll({
-            include:[{
-                model:User,
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [{
+                model: User,
                 attributes: ['username'],
             }],
         });
         console.log(postData);
-        const posts = postData.map((post) =>post.get({ plain: true }));
-        
-        res.status(200).render('homepage',{
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        res.status(200).render('homepage', {
             posts,
         });
 
-    }catch(err){
+    } catch (err) {
         res.status(500).json(err);
     }
 });
 
-router.get('/post/:id',async(req,res)=>{
-    try{
-        const postData=await Post.findByPk(req.params.id,{
-            include:[{model:User}],
+router.get('/post/:id', async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: [{ model: User }],
         });
-        if(!postData){
-            res.status(404).json("no Found data");
+        if (!postData) {
+            res.status(404).json("No Found data");
         }
-        const post=await postData.map((post)=>post.get({plain:true}));
-        res.status(200).render('post',{
-            post,
-        });
-        
+        const post = postData.get({ plain: true });
+        res.status(200).render('post', { post });
 
-    }catch(err){
+    } catch (err) {
         res.status(500).json(err);
     }
 })
 
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
-      res.redirect('/');
-      return;
+        res.redirect('/');
+        return;
     }
-  
     res.render('login');
-  });
+});
 
 module.exports = router;
