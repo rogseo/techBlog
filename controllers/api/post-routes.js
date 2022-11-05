e5c4558915cf38ffcbcc2a086a8af06cfaac5774
rogseo <rogseo@gmail.com>
2022-11-05 02:16:30 -0500

    successfully edit post but getting 500 response

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,30 +1,30 @@
 const router = require('express').Router();
 const { Post } = require('../../models');
-const withAuth=require('../../utils/withAuth')
+const withAuth = require('../../utils/withAuth')
 
 // CREATE new post
 router.get('/', withAuth, async (req, res) => {
-    try {
-      const dbPostData = await Post.findAll({
-        where:{
-          user_id:req.session.user_id,
-        }
-        });
-      if(!dbPostData){
-          res.json("no found data");
+  try {
+    const dbPostData = await Post.findAll({
+      where: {
+        user_id: req.session.user_id,
       }
-      console.log(dbPostData);
-      const posts = dbPostData.map((post)=>post.get({ plain: true }));
-      res.status(200).render('dashboard', { posts,loggedIn: req.session.loggedIn});
-
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
+    });
+    if (!dbPostData) {
+      res.json("no found data");
     }
-  });
+    console.log(dbPostData);
+    const posts = dbPostData.map((post) => post.get({ plain: true }));
+    res.status(200).render('dashboard', { posts, loggedIn: req.session.loggedIn });
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 
-router.post('/',withAuth, async (req, res) => {
+router.post('/', withAuth, async (req, res) => {
   try {
     console.log(req.session.user_id);
     const dbPostData = await Post.create({
@@ -33,11 +33,11 @@ router.post('/',withAuth, async (req, res) => {
       // post_date: req.body.post_date,
       user_id: req.session.user_id,
     });
-    if(!dbPostData){
-        res.json("no added");
+    if (!dbPostData) {
+      res.json("no added");
     }
-    else{
-        res.status(200).json(dbPostData);
+    else {
+      res.status(200).json(dbPostData);
     }
   } catch (err) {
     console.log(err);
@@ -45,18 +45,46 @@ router.post('/',withAuth, async (req, res) => {
   }
 });
 
-router.get("/:id",withAuth,async(req,res)=>{
-  const postData=await Post.findByPk(req.params.id);
-  const post=postData.get({plain:true});
-  res.status(200).render('editPost',{
+router.get("/:id", withAuth, async (req, res) => {
+  const postData = await Post.findByPk(req.params.id);
+  const post = postData.get({ plain: true });
+  res.status(200).render('editPost', {
     post,
     loggedIn: req.session.loggedIn,
-    user_id:req.session.user_id,
+    user_id: req.session.user_id,
   });
 
-})
+});
+
+router.put("/:id", withAuth, async (req, res) => {
+  try {
+    const postData = await Post.update(
+      {
+        title: req.body.title,
+        description: req.body.description,
+        user_id: req.session.user_id,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      });
+
+    if (!postData[0]) {
+      res.status(404).json({ message: 'No post with this id!' });
+      return;
+    }
+    const post = postData.get({ plain: true });
+    res.status(200).render('post', {
+      post,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 //if add post button click
-router.get('/add',withAuth,async(req,res)=>{
+router.get('/add', withAuth, async (req, res) => {
   res.status(200).render('addPost');
 
 });
